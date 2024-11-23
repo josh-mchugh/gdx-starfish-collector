91e49e5480658bfc4569ed90814e0fe670cc472d
Josh McHugh <josh.mchugh@mailbox.org>
2024-11-23 00:17:21 -0500

    Followed the first example

--- core/src/main/scala/net/sailware/starfish/Main.scala
+++ core/src/main/scala/net/sailware/starfish/Main.scala
@@ -1,25 +1,75 @@
 package net.sailware.starfish
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.Input.Keys
 
 class Main extends ApplicationAdapter:
 
   var batch: SpriteBatch = null
-  var image: Texture = null
+
+  var turtleTexture: Texture = null
+  var turtleRectangle: Rectangle = null
+  var turtleX = 0F
+  var turtleY = 0F
+
+  var starfishTexture: Texture = null
+  var starfishRectangle: Rectangle = null
+  var starfishX = 0F
+  var starfishY = 0F
+
+  var oceanTexture: Texture = null
+
+  var winMessageTexture: Texture = null
+
+  var win = false
 
   override def create(): Unit =
     batch = new SpriteBatch()
-    image = new Texture("libgdx.png")
+    turtleTexture = new Texture("turtle-1.png")
+    turtleX = 20F
+    turtleY = 20F
+    turtleRectangle = new Rectangle(turtleX, turtleY, turtleTexture.getWidth().toFloat, turtleTexture.getHeight().toFloat)
+
+    starfishTexture = new Texture("starfish.png")
+    starfishX = 380F
+    starfishY = 380F
+    starfishRectangle = new Rectangle(starfishX, starfishY, starfishTexture.getWidth().toFloat, starfishTexture.getHeight().toFloat)
+
+    oceanTexture = new Texture("water.jpg")
+
+    winMessageTexture = new Texture("you-win.png")
 
   override def render(): Unit =
-    ScreenUtils.clear(0.15F, 0.15F, 0.2F, 1F)
+    if (Gdx.input.isKeyPressed(Keys.LEFT))
+        turtleX -= 1
+    if (Gdx.input.isKeyPressed(Keys.RIGHT))
+        turtleX += 1
+    if (Gdx.input.isKeyPressed(Keys.UP))
+        turtleY += 1
+    if (Gdx.input.isKeyPressed(Keys.DOWN))
+      turtleY -= 1
+
+    turtleRectangle.setPosition(turtleX, turtleY)
+
+    if (turtleRectangle.overlaps(starfishRectangle))
+      win = true
+
+    ScreenUtils.clear(0, 0, 0, 1F)
     batch.begin()
-    batch.draw(image, 140, 210)
+    batch.draw(oceanTexture, 0, 0)
+    if (!win)
+      batch.draw(starfishTexture, starfishX, starfishY)
+    else
+      batch.draw(winMessageTexture, 180, 180)
+    batch.draw(turtleTexture, turtleX, turtleY)
     batch.end()
 
   override def dispose(): Unit =
     batch.dispose()
-    image.dispose()
+    turtleTexture.dispose()
+    oceanTexture.dispose()
